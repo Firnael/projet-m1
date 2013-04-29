662e0b67b766d8a24b26e99465a9ac7edeac28e3
Firnael <audren.burlot@gmail.com>
2013-04-29 14:36:55 +0200

    - Updated init() of Inventory.js - Updated Inventory modal - Centered all modal titles

--- app/gameClasses/Inventory.js
+++ app/gameClasses/Inventory.js
@@ -6,9 +6,16 @@ var Inventory = IgeEntityBox2d.extend({
         self.weapon = new Weapon(1);
         self.crops = {};
         self.seeds = {};
-        self.money = 0;
+        self.money = 100;
         self.fertilizerUnits = 0;
         self.waterUnits = 0;
+
+        self.crops["wheat"] = 0;
+        self.crops["tomato"] = 0;
+        self.crops["corn"] = 0;
+        self.seeds["wheat"] = 0;
+        self.seeds["tomato"] = 0;
+        self.seeds["corn"] = 0;
     },
 
     setWeapon: function (type) {
